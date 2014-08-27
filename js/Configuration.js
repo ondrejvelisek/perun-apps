a7513e35d76f3565d44ca8ad5408aacb711ba07b
Ondrej <Ondrej@10.0.0.36>
2014-08-27 14:11:36 +0200

    ready to JavaScript

--- js/Configuration.js
+++ js/Configuration.js
@@ -1,7 +1,7 @@
 Configuration = {
 
-    RPC_URL : "https://einfra.cesnet.cz/fed/rpc/jsonp/",
-    TEST_RPC_URL : "https://einfra.cesnet.cz/fed/rpc/",
+    RPC_URL : "https://perun-dev.meta.zcu.cz/krb/rpc/jsonp/",
+    TEST_RPC_URL : "https://perun-dev.meta.zcu.cz/krb/rpc/",
     
     LOADER_IMAGE : "<img src=\"/img/ajax-loader.gif\" alt=\"loading\" />"
-}
+};
