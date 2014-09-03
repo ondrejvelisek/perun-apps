d2849294dde299c4a4616ac64c0096c5d6e5863f
Ondrej <ondrejvelisek@gmail.com>
2014-09-03 18:45:02 +0200

    complete

---
+++ perun-user-profile/js/Projects.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+    $("#projectsLink").click(function() { 
+       loadProjects(user);
+    });
+});
+    
+function loadProjects(user) {
+    if (!user) {
+        (new Message("Projects","can't be loaded because user isn't loaded.","error")).draw();
+        return;
+    }
+    var loadImage = new LoadImage($("#projects-table"), "auto");
+    
+    callPerun("usersManager", "getVosWhereUserIsMember", {user: user.id}, function(projects) {
+        if (!projects) {
+            (new Message("Projects","can't be loaded.","error")).draw();
+            return;
+        }
+        fillProjects(projects);
+        loadImage.hide();
+        //(new Message("Projects","was loaded successfully.","success"));
+    });
+}
+
+function fillProjects(projects) {
+    if (!projects) {
+        (new Message("Projects","can't be fill.","error")).draw();
+        return;
+    }
+    var projectTable = new PerunTable();
+    projectTable.addColumn("name", "Projects");
+    projectTable.setValues(projects);
+    var tableHtml = projectTable.draw();
+    $("#projects-table").html(tableHtml);
+}
