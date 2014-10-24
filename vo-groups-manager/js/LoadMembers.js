f95b34606a0d6514002032a213ecc9981f71f339
Ondrej <ondrejvelisek@gmail.com>
2014-10-24 15:46:16 +0200

    remove member from table

--- vo-groups-manager/js/LoadMembers.js
+++ vo-groups-manager/js/LoadMembers.js
@@ -45,24 +45,39 @@ function fillMembers(members, group) {
         var attrs = members[i].userAttributes;
         for (var j in attrs) {
             users[i][attrs[j].friendlyName] = attrs[j].value;
-            
+
         }
     }
 
     var membersTable = new PerunTable();
     //membersTable.setClicableRows({isClicable : true, id:"id", prefix:"row-"});
     //membersTable.addColumn({type: "number", title: "#"});
-    membersTable.addColumn({type: "icon", title: "", name: "membershipTypeIcon", description: "is direct member" });
+    membersTable.addColumn({type: "icon", title: "", name: "membershipTypeIcon", description: "is direct member"});
     membersTable.addColumn({type: "text", title: "Name", name: "displayName"});
     membersTable.addColumn({type: "text", title: "Preferred Mail", name: "preferredMail"});
     membersTable.addColumn({type: "button", title: "", btnText: "&times;", btnId: "id", btnName: "removeMember", btnType: "danger"});
     membersTable.setValues(users);
     table.html(membersTable.draw());
-    
+
     table.find('[data-toggle="tooltip"]').tooltip();
-    table.find('button[id^=removeMember]').click(function() {
-        var memberId = $(this).attr("id");;
-        debug(memberId);
+    table.find('button[id^=removeMember]').click(function () {
+        var memberId = $(this).attr("id").split("-")[1];
+
+        callPerunPost("groupsManager", "removeMember", {group: group.id, member: members[j].id},
+        function () {
+            (flowMessager.newMessage(member.name, "was removed sucesfuly from " + group.shortName + " group", "success")).draw();
+            showGroup(group.id);
+            refreshAllParentsMembers(group);
+        }, function (error) {
+            switch (error.name) {
+                case "NotGroupMemberException":
+                    (flowMessager.newMessage(member.name, "is not in in group " + group.shortName, "warning")).draw();
+                    break;
+                default:
+                    (flowMessager.newMessage("Internal error", "Can not remove member " + member.name + " from group " + group.shortName, "danger")).draw();
+                    break;
+            }
+        });
     });
 }
 
