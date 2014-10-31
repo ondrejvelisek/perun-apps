95d059ac35630dd9adde7efa9191ae8cb3692f7c
Ondrej <ondrejvelisek@gmail.com>
2014-10-31 16:05:03 +0100

    send plain/text mail

--- establish-vo-form/js/EstablishForm.js
+++ establish-vo-form/js/EstablishForm.js
@@ -12,21 +12,19 @@ $(document).ready(function() {
     $("form#establishForm").submit(function(e) {
         e.preventDefault();
         var form = $("form#establishForm");
-        var queue = null;
+        var queue = "perun";
         var subject = "Application for establish new VO";
         var text = getTextFromForm(form);
         form[0].reset();
-        (staticMessager.newMessage(subject+"<br><br>", text, "default")).draw();
-        /*
-        callPerunPost("rtMessagesManager", "sentMessageToRT", {queue: queue, subject: subject, text: text}, function() {
-            (flowMessager.newMessage("Application ", "was send successfully", "success")).draw();
+        /*callPerunPost("rtMessagesManager", "sentMessageToRT", {queue: queue, subject: subject, text: text}, function() {
+            (flowMessager.newMessage("Application", "was send successfully", "success")).draw();
         });*/
         
         function getTextFromForm(form) {
             var text = "";
             form.find("input, textarea").each(function() {
                 text += form.find("label[for="+$(this).attr("id")+"]").text()+": ";
-                text += "<strong>"+$(this).val()+"</strong><br>"
+                text += $(this).val()+"  \r\n";
             });
             return text;
         }
