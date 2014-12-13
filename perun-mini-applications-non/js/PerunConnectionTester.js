cf98d9e089b94c033157ffb9b65fcc96d86100dc
Ondrej <ondrejvelisek@gmail.com>
2014-12-13 16:17:54 +0100

    change project structure

---
+++ perun-mini-applications-non/js/PerunConnectionTester.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function() {
+    if (configuration.TESTING_CONNECTION) {
+        // Methods which check if the Perun connection is OK
+        setTimeout(executeQuery, 5000);
+    }
+});
+
+
+function reloadMsg() {
+    alert("Data connection lost. Click OK to relad the page.");
+    window.location.reload();
+}
+
+
+function executeQuery() {
+        $.ajax({
+            url: configuration.TEST_RPC_URL,
+            success: function(data) {
+                if (!(data.indexOf("OK!") == 0)) {
+                    reloadMsg();
+                }
+                setTimeout(executeQuery, 5000);
+            },
+            statusCode: {
+                404: function() {
+                    reloadMsg();
+                },
+                401: function() {
+                    reloadMsg();
+                },
+                403: function() {
+                    reloadMsg();
+                },
+                302: function() {
+                    window.location.reload();
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                reloadMsg();
+            }
+        });
+}
