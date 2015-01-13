474e90ef6106a75ce6de8d136ecd12afaf119bca
ondrej <ondrejvelisek@gmail.com>
2015-01-13 13:04:54 +0100

    fix missing config file

---
+++ establish-vo-form/js/Configuration.js
@@ -0,0 +1,7 @@
+configuration = {
+    RPC_URL: "http://localhost/krb/rpc/jsonp/",
+    TEST_RPC_URL: "http://localhost/krb/rpc/",
+    TESTING_CONNECTION: false, //because localhost hasnt got access and throws error
+
+    LOADER_IMAGE: '../../img/loader_white-bg.gif'
+};
